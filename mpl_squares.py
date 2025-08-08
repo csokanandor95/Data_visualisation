f5b5784124ff183ab005dfdb27f85d509d4c29a5
csokanandor95 <csokanandor77@gmail.com>
2025-08-08 09:22:38 +0200

    Update mpl_squares.py

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,8 +1,11 @@
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt # pyplot module contains functions that help generate charts and plots
 
 squares = [1,4,9,16,25]
 
-fig, ax = plt.subplots()
-ax.plot(squares)
+# fig is the entire figure
+# ax is a single plot in the figure - used mostly for customizing
 
-plt.show()
+fig, ax = plt.subplots() # this function generates plots in the same figure
+ax.plot(squares) # plot method tries to plot the data given
+
+plt.show() # opens Matplotlib's viewer and displays the plot
