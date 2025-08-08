c639c505453bd4f4f52d4bc98fb55921005ce44a
csokanandor95 <csokanandor77@gmail.com>
2025-08-08 09:33:42 +0200

    Enhance plot with titles, labels, and styling
    Added a chart title, axis labels, and increased line width for better visualization. Also set the size of tick labels to improve readability.

--- mpl_squares.py
+++ mpl_squares.py
@@ -4,8 +4,15 @@ squares = [1,4,9,16,25]
 
 # fig is the entire figure
 # ax is a single plot in the figure - used mostly for customizing
-
 fig, ax = plt.subplots() # this function generates plots in the same figure
-ax.plot(squares) # plot method tries to plot the data given
+ax.plot(squares, linewidth=3) # plot method tries to plot the data given
+
+# set chart title and label axes
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+#set size of tick labels
+ax.tick_params(labelsize=14)
 
 plt.show() # opens Matplotlib's viewer and displays the plot
