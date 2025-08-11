1517c5b411116aceee349e4b9f3e74753f69d4c3
csokanandor95 <csokanandor77@gmail.com>
2025-08-11 21:06:39 +0200

    Plot 5000 cubic numbers with color mapping
    Expanded the scatter plot to display cubes of numbers from 1 to 5000, using color mapping based on y-values and scientific notation for axis labels. Adjusted marker size and improved axis formatting for better visualization.

--- colored_cubes.py
+++ colored_cubes.py
@@ -3,13 +3,17 @@ import matplotlib.pyplot as plt
 plt.style.use('seaborn-v0_8')
 fig,ax = plt.subplots()
 
-x_values = [1,2,3,4,5]
-y_values = [1,8,27,64,125]
+x_values = range(1, 5001)
+y_values = [x**3 for x in x_values]
 
-ax.scatter(x_values, y_values, s=100)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 ax.set_title("Cubic Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Cubes", fontsize=14)
+
+ax.tick_params(labelsize=14)
+
+ax.ticklabel_format(style='scientific')
 
 plt.show()
