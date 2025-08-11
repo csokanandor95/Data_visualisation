d2d6f58c5ee9dca132f2f2b3f1663f1125779990
csokanandor95 <csokanandor77@gmail.com>
2025-08-11 20:32:26 +0200

    Plot 1000 square numbers with adjusted axis and size
    Updated the scatter plot to display squares of numbers from 1 to 1000, reduced the marker size for better visualization, and set explicit axis ranges to accommodate the larger dataset.

--- scatter_squares.py
+++ scatter_squares.py
@@ -9,10 +9,10 @@ fig,ax = plt.subplots()
 #ax.scatter(2,4,s=200) # s = size of data point
 
 # plotting series of points with scatter()
-x_values = [1,2,3,4,5]
-y_values = [1,4,9,16,25]
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
 
-ax.scatter(x_values, y_values, s=100)
+ax.scatter(x_values, y_values, s=10)
 
 # set chart title and label axes
 ax.set_title("Square Numbers", fontsize=24)
@@ -21,5 +21,8 @@ ax.set_ylabel("Square of Value", fontsize=14)
 
 # set size of tick labels
 ax.tick_params(labelsize=14)
+
+#set the range for each axis
+ax.axis([0, 1100, 0, 1_000_000])
 
 plt.show()
