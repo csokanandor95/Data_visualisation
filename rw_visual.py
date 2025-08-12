3804afb559a881d0b00e8c30528b066d2d12158d
csokanandor95 <csokanandor77@gmail.com>
2025-08-12 09:20:16 +0200

    Add visualization script for random walk
    Introduces rw_visual.py, which generates and displays a random walk using matplotlib. The script creates a RandomWalk instance, fills it, and plots the points with equal axis aspect.

---
+++ rw_visual.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+from random_walk import RandomWalk
+
+# make a random walk
+
+rw = RandomWalk()
+rw.fill_walk()
+
+# plot the points in the walk
+plt.style.use('classic')
+fig, ax = plt.subplots()
+
+ax.scatter(rw.x_values, rw.y_values, s=15)
+ax.set_aspect('equal') # both axes have equal spacing between tick marks
+
+plt.show()
