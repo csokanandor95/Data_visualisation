69614535d2ed0d665da5bd4609d04ff5ee14928b
csokanandor95 <csokanandor77@gmail.com>
2025-08-12 09:43:18 +0200

    Add loop to allow multiple random walks
    Refactored the script to repeatedly generate and display random walks until the user chooses to exit. This allows for interactive exploration of different random walk patterns.

--- rw_visual.py
+++ rw_visual.py
@@ -2,16 +2,24 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-# make a random walk
+# keep making random walks as long as the program is active
 
-rw = RandomWalk()
-rw.fill_walk()
+while True:
 
-# plot the points in the walk
-plt.style.use('classic')
-fig, ax = plt.subplots()
+    # make a random walk
 
-ax.scatter(rw.x_values, rw.y_values, s=15)
-ax.set_aspect('equal') # both axes have equal spacing between tick marks
+    rw = RandomWalk()
+    rw.fill_walk()
 
-plt.show()
+    # plot the points in the walk
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+
+    ax.scatter(rw.x_values, rw.y_values, s=15)
+    ax.set_aspect('equal') # both axes have equal spacing between tick marks
+
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
