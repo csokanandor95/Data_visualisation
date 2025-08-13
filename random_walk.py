46eb8c834c38eeccbc99d83f17cfa97d7e1e8a77
csokanandor95 <csokanandor77@gmail.com>
2025-08-13 22:01:33 +0200

    Refactor step calculation in RandomWalk
    Extracted step calculation logic into a new get_step() method to reduce code duplication and improve readability in the fill_walk method.

--- random_walk.py
+++ random_walk.py
@@ -13,26 +13,28 @@ class RandomWalk:
 
     def fill_walk(self):
         """Calculate all the points in the walk."""
-
-        # keep taking steps until the walk reaches the desired steps
+        # Keep taking steps until the walk reaches the desired length.
         while len(self.x_values) < self.num_points:
 
-            # decide which direction to go and how far to go
-            x_direction = choice([1, -1]) # 1 is right, -1 is left
-            x_distance = choice([0, 1, 2, 3, 4, 5, 6, 7, 8])
-            x_step = x_direction * x_distance # pos. is right, neg.is left and 0 is vertical movement
+            # Decide which direction to go, and how far to go.
+            x_step = self.get_step()
+            y_step = self.get_step()
 
-            y_direction = choice([1, -1])
-            y_distance = choice([0, 1, 2, 3, 4])
-            y_step = y_direction * y_distance # pos. is up, neg.is down and 0 is horizontal movement
-
-            # reject moves that go nowhere
+            # Reject moves that go nowhere.
             if x_step == 0 and y_step == 0:
                 continue
 
-            # calculate the new position
+            # Calculate the new position.
             x = self.x_values[-1] + x_step
             y = self.y_values[-1] + y_step
 
             self.x_values.append(x)
             self.y_values.append(y)
+
+    def get_step(self):
+        """Calculate a single step in the walk."""
+        direction = choice([1, -1])
+        distance = choice([0, 1, 2, 3, 4])
+        step = direction * distance
+
+        return step
