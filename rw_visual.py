e3a86aa1302d46ee86a836a2766649cc0ee0e249
csokanandor95 <csokanandor77@gmail.com>
2025-08-13 21:39:47 +0200

    Highlight start/end points and hide axes in plot
    Added green and red markers to emphasize the starting and ending points of the random walk. Also removed axis visibility for a cleaner visualization.

--- rw_visual.py
+++ rw_visual.py
@@ -19,6 +19,16 @@ while True:
     ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
     ax.set_aspect('equal') # both axes have equal spacing between tick marks
 
+    # emphasize the first and last points
+
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100) # starting point in green
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100) # ending point in green
+
+    # remove axes
+
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
