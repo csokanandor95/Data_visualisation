52026fca82ab6bfe7ee823dd6164660b9a4d8dc7
csokanandor95 <csokanandor77@gmail.com>
2025-08-18 11:57:49 +0200

    Add matplotlib plot for Sitka high temperatures
    Integrated matplotlib to visualize daily high temperatures for July 2021. The script now generates a line plot with appropriate labels and styling.

--- sitka_highs.py
+++ sitka_highs.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import csv
+import matplotlib.pyplot as plt
 
 path = Path('weather_data/sitka_weather_07-2021_simple.csv')
 lines = path.read_text(encoding='utf-8').splitlines() # creates list of lines
@@ -13,4 +14,15 @@ for row in reader:
     high = int(row[4])
     highs.append(high)
 
-print(highs)
+# plot hight temperatures
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots()
+ax.plot(highs, color='red')
+
+# format plot
+ax.set_title("Daily high temperatures, July 2021", fontsize=24)
+ax.set_xlabel('', fontsize=16)
+ax.set_ylabel("Temperature (F)", fontsize=16)
+ax.tick_params(labelsize=16)
+
+plt.show()
