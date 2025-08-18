ba0f26a133d0391c3cb5a2bb1ab49e789229ced9
csokanandor95 <csokanandor77@gmail.com>
2025-08-18 11:41:37 +0200

    Add script to read Sitka weather CSV header
    Added sitka_highs.py to read and print the header row from the Sitka July 2021 weather CSV file. Included the sample CSV data in weather_data/sitka_weather_07-2021_simple.csv for use with the script.

---
+++ sitka_highs.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+import csv
+
+path = Path('weather_data/sitka_weather_07-2021_simple.csv')
+lines = path.read_text(encoding='utf-8').splitlines() # creates list of lines
+
+reader = csv.reader(lines)
+header_row = next(reader)
+print(header_row)
