1346cc11a97070a472730293ac9518d70cac984e
csokanandor95 <csokanandor77@gmail.com>
2025-08-18 15:53:09 +0200

    Plot daily highs with dates on x-axis
    Updated the script to parse dates from the CSV and plot high temperatures against dates instead of index. Added date formatting for the x-axis to improve readability.

--- sitka_highs.py
+++ sitka_highs.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import csv
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 path = Path('weather_data/sitka_weather_07-2021_simple.csv')
 lines = path.read_text(encoding='utf-8').splitlines() # creates list of lines
@@ -8,20 +9,23 @@ lines = path.read_text(encoding='utf-8').splitlines() # creates list of lines
 reader = csv.reader(lines)
 header_row = next(reader)
 
-# extract high temperatures
-highs = []
+# extract date and high temperatures
+dates, highs = [], []
 for row in reader:
+    current_date = datetime.strptime(row[2], '%Y-%m-%d')
     high = int(row[4])
+    dates.append(current_date)
     highs.append(high)
 
 # plot hight temperatures
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
-ax.plot(highs, color='red')
+ax.plot(dates, highs, color='red')
 
 # format plot
 ax.set_title("Daily high temperatures, July 2021", fontsize=24)
 ax.set_xlabel('', fontsize=16)
+fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F)", fontsize=16)
 ax.tick_params(labelsize=16)
 
