f6581d6098619d15771eb46c6f2665cf35627929
csokanandor95 <csokanandor77@gmail.com>
2025-08-18 16:31:48 +0200

    Add plotting of Death Valley 2021 temperature data
    Extracts dates, highs, and lows from the CSV file and plots daily high and low temperatures for Death Valley in 2021 using matplotlib. Handles missing data and improves visualization with labels and formatting.

--- death_valley_highs_lows.py
+++ death_valley_highs_lows.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import csv
+import matplotlib.pyplot as plt
+from datetime import datetime
 
 path = Path('weather_data/death_valley_2021_simple.csv')
 lines = path.read_text(encoding='utf-8').splitlines() # creates list of lines
@@ -7,5 +9,32 @@ lines = path.read_text(encoding='utf-8').splitlines() # creates list of lines
 reader = csv.reader(lines)
 header_row = next(reader)
 
-for index, column_header in enumerate(header_row):
-    print(index, column_header)
+# extract dates and high and low temperatures
+dates, highs, lows = [], [], []
+for row in reader:
+    current_date = datetime.strptime(row[2], '%Y-%m-%d')
+    try:
+        high = int(row[3])
+        low = int(row[4])
+    except ValueError:
+        print(f"Missing data for {current_date}")
+    else:
+        dates.append(current_date)
+        highs.append(high)
+        lows.append(low)
+
+# plot high and low temperatures
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots()
+ax.plot(dates, highs, color='red', alpha=0.5) # alpha is the colors transparency
+ax.plot(dates, lows, color='blue', alpha=0.5)
+ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+
+# format plot
+ax.set_title("Daily high and low temperatures, 2021\nDeath Valley, CA", fontsize=20)
+ax.set_xlabel('', fontsize=16)
+fig.autofmt_xdate()
+ax.set_ylabel("Temperature (F)", fontsize=16)
+ax.tick_params(labelsize=16)
+
+plt.show()
