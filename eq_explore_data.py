36a8e63fe3dcfc125a22e51445c49a9f9e79d039
csokanandor95 <csokanandor77@gmail.com>
2025-08-20 11:30:20 +0200

    Representing magnitudes

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -4,7 +4,7 @@ import json
 import plotly.express as px
 
 # read data as a string and convert to a python object
-path = Path('eq_data/eq_data_1_day_m1.geojson')
+path = Path('eq_data/eq_data_30_day_m1.geojson')
 contents = path.read_text(encoding='utf-8')
 all_eq_data = json.loads(contents)
 
@@ -26,5 +26,10 @@ for eq_dict in all_eq_dicts:
     lats.append(lat)
 
 title = 'Global Earthquakes'
-fig = px.scatter_geo(lat=lats, lon=lons, title=title)
+fig = px.scatter_geo(lat=lats, lon=lons, size=mags, title=title,
+        color=mags,
+        color_continuous_scale='Viridis',
+        labels={'color':'Magnitude'},
+        projection='natural earth',
+    )
 fig.show()
